4859b2c71b47f29751f846fdf261e0ee97a12471
Suryagayathri K P <suryagayathrikp@gmail.com>
2024-03-14 12:40:27 +0530

    updated settings appearance

--- app/(main)/billing/page.tsx
+++ app/(main)/billing/page.tsx
@@ -88,30 +88,7 @@ const Billing = () => {
           </TableRow>
         </TableHeader>
         <TableBody>
-          <TableRow>
-            <TableCell className="font-medium">INV001</TableCell>
-            <TableCell>Paid</TableCell>
-            <TableCell>Credit Card</TableCell>
-            <TableCell className="text-right">$250.00</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell className="font-medium">INV001</TableCell>
-            <TableCell>Paid</TableCell>
-            <TableCell>Credit Card</TableCell>
-            <TableCell className="text-right">$250.00</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell className="font-medium">INV001</TableCell>
-            <TableCell>Paid</TableCell>
-            <TableCell>Credit Card</TableCell>
-            <TableCell className="text-right">$250.00</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell className="font-medium">INV001</TableCell>
-            <TableCell>Paid</TableCell>
-            <TableCell>Credit Card</TableCell>
-            <TableCell className="text-right">$250.00</TableCell>
-          </TableRow>
+          <TableRow></TableRow>
         </TableBody>
       </Table>
     </div>
--- app/(main)/settings/appearance/page.tsx
+++ app/(main)/settings/appearance/page.tsx
@@ -1,6 +1,23 @@
+import { Switch } from "@/components/switch";
+import Header from "../../_components/Header";
+
 const Appearance = () => {
-    return (
-        <div>Appearance</div>
-    )
+  return (
+    // <div className="flex w-full flex-col gap-10 p-8 pl-10 align-middle">
+    //   <div className="flex gap-96">
+    //     <div className="text-lg">Dark Mode</div>
+    //     <Switch />
+    //   </div>
+    //   <div className="h-7 cursor-pointer text-xl font-semibold ">Themes</div>
+    // </div>
+    <>
+      <Header title="Themes" />
+      <hr />
+      <div className="flex gap-96 p-8">
+        <div className="text-lg">Dark Mode</div>
+        <Switch />
+      </div>
+    </>
+  );
 };
-export default Appearance
+export default Appearance;
